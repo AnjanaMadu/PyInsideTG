06b921e1412b1a6352769f76dfb925aec986edb1
Amarnath C <amarnathcharichilil@gmail.com>
2021-08-04 17:48:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ async def updateE(event):
         return
     try:
      repo = Repo()
-    except InvalidGitRepositoryError as e:
+    except InvalidGitRepositoryError:
      repo = Repo.init()
-     origin = repo.create_remote("upstream", "https://github.com/AnjanaMadu/TerminalBot.git")
+     origin = repo.create_remote("upstream", "https://github.com/AnjanaMadu/TerminalBot")
      origin.fetch()
      repo.create_head("master", origin.refs.master)
      repo.heads.master.set_tracking_branch(origin.refs.master)
      repo.heads.master.checkout(True)
-    repo.create_remote("upstream", 'https://github.com/AnjanaMadu/TerminalBot.git')
+    repo.create_remote("upstream", 'https://github.com/AnjanaMadu/TerminalBot')
     ac_br = repo.active_branch.name
     ups_rem = repo.remote("upstream")
     ups_rem.fetch(ac_br)
