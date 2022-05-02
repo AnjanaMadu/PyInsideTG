a2eb2e71a6ae2ad3d0944de692ca58b5efa8f45a
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2022-05-02 21:45:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,186 +1,144 @@
-import os, logging, asyncio, io, sys, traceback
-from datetime import datetime
-from telethon import TelegramClient, events
-from telethon.sessions import StringSession
-from git import Repo
-from os import environ, execle
+import io
+import os
 import sys
-from git.exc import GitCommandError, InvalidGitRepositoryError
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(name)s - [%(levelname)s] - %(message)s'
-)
-LOGGER = logging.getLogger(__name__)
+import json
+import asyncio
+import traceback
+from telethon import TelegramClient, events
 
-# --- STARTING BOT --- #
-api_id = int(os.environ.get("APP_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("TOKEN")
-auth_chts = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-banned_usrs = set(int(x) for x in os.environ.get("BANNED_USRS", "").split())
-client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+class Database:
+    def __init__(self):
+        self.db = {}
+        if not os.path.exists("db.json"):
+            with open("db.json", "w") as f:
+                f.write("{}")
+        else:
+            with open("db.json", "r") as f:
+                self.db = json.load(f)
 
-# --- BAN --- #
-@client.on(events.NewMessage(pattern="/ban"))
-async def banE(event):
-    k = await event.get_reply_message()
-    banned_usrs.append(k.from_id.user_id)
-# --- PINGING BOT --- #
-@client.on(events.NewMessage(pattern="/ping"))
-async def pingE(event):
-    start = datetime.now()
-    catevent = await event.respond("`!....`")
-    await asyncio.sleep(0.3)
-    await catevent.edit("`..!..`")
-    await asyncio.sleep(0.3)
-    await catevent.edit("`....!`")
-    end = datetime.now()
-    tms = (end - start).microseconds / 1000
-    ms = round((tms - 0.6) / 3, 3)
-    await catevent.edit(f"Pong!\n`{ms} ms`")
-    LOGGER.info("Bot Pinging")
+    def get(self, key):
+        return self.db.get(key, None)
 
-# --- UPDATE BOT --- #
-@client.on(events.NewMessage(pattern="/update"))
-async def updateE(event):
-    if not event.sender_id in auth_chts:
-        return
-    try:
-     repo = Repo()
-    except InvalidGitRepositoryError:
-     repo = Repo.init()
-     origin = repo.create_remote("upstream", "https://github.com/AnjanaMadu/TerminalBot")
-     origin.fetch()
-     repo.create_head("master", origin.refs.master)
-     repo.heads.master.set_tracking_branch(origin.refs.master)
-     repo.heads.master.checkout(True)
-    repo.create_remote("upstream", 'https://github.com/AnjanaMadu/TerminalBot')
-    ac_br = repo.active_branch.name
-    ups_rem = repo.remote("upstream")
-    ups_rem.fetch(ac_br)
-    try:
-            ups_rem.pull(ac_br)
-    except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-    args = [sys.executable, "main.py"]
-    execle(sys.executable, *args, environ)
+    def set(self, key, value):
+        self.db[key] = value
+        with open("db.json", "w") as f:
+            json.dump(self.db, f)
 
+    def isOwner(self, user):
+        return user == self.get("OWNER")
 
-    
-# --- RESTART BOT --- #
-@client.on(events.NewMessage(pattern="/restart"))
-async def restartE(event):
-    if not event.sender_id in auth_chts:
-        return
-    await event.respond("Restarting")
-    executable = sys.executable.replace(" ", "\\ ")
-    args = [executable, "main.py"]
-    os.execle(executable, *args, os.environ)
-    sys.exit(0)
-    LOGGER.info("Bot Restarting")
+    def isAuth(self, user):
+        return user in self.get("AUTH") or self.isOwner(user)
 
-# --- EVAL DEF HERE --- #
-async def aexec(code, smessatatus):
-    message = event = smessatatus
-    p = lambda _x: print(_format.yaml_format(_x))
-    reply = await event.get_reply_message()
+def getEnv(key):
+    value = os.environ.get(key, None)
+    if value is None:
+        raise KeyError(f"{key} is not set")
+        sys.exit(1)
+    return value
+
+async def aexec(code, event):
     exec(
-        f"async def __aexec(message, event , reply, client, p, chat): "
-        + "".join(f"\n {l}" for l in code.split("\n"))
+        'async def __aexec(event, client): '
+        + '\n reply = await event.get_reply_message()'
+        + '\n chat = event.chat_id'
+        + ''.join(f'\n {l}' for l in code.split('\n'))
     )
-    return await locals()["__aexec"](
-        message, event, reply, message.client, p, message.chat_id
-    )
- 
-# --- EVAL EVENT HERE --- # 
-@client.on(events.NewMessage(chats=auth_chts, pattern="/eval ?(.*)"))
-async def evalE(event):
-    if event.sender_id in banned_usrs:
-        return await event.respond("You are Banned!")
-    cmd = "".join(event.message.message.split(maxsplit=1)[1:])
-    if not cmd:
+    return await locals()['__aexec'](event, event.client)
+
+db = Database()
+db.set("OWNER", int(getEnv("OWNER_ID")))
+auth_ = getEnv("AUTH_USERS").split(" ")
+db.set("AUTH", [int(i) for i in auth_])
+client = TelegramClient("evalbot", api_id=int(getEnv("API_ID")), api_hash=getEnv("API_HASH"))
+
+@client.on(events.NewMessage(pattern="^/auth$"))
+async def auth_actions(event):
+    if not db.isOwner(event.sender_id):
         return
-    cmd = (
-        cmd.replace("send_message", "send_message")
-        .replace("send_file", "send_file")
-        .replace("edit_message", "edit_message")
-    )
-    catevent = await event.respond("`Running ...`")
+    auth = list(db.get("AUTH"))
+    reply = await event.get_reply_message()
+    if reply is None:
+        ulist = "\n".join(f"`{i}`" for i in auth)
+        await event.reply("Auth list:\n" + ulist)
+        return
+    if reply.sender_id in auth:
+        auth.remove(reply.sender_id)
+        db.set("AUTH", auth)
+        await event.reply(f"Removed `{reply.sender_id}` from auth list")
+    else:
+        auth.append(reply.sender_id)
+        db.set("AUTH", auth)
+        await event.reply(f"Added `{reply.sender_id}` to auth list")
+
+@client.on(events.NewMessage(pattern="^/restart$"))
+async def restart_action(event):
+    if not db.isOwner(event.sender_id):
+        return
+    await event.reply("Restarting...")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    sys.exit(0)
+
+@client.on(events.NewMessage(pattern="^/(bash|sh)"))
+async def bash_action(event):
+    if not db.isAuth(event.sender_id):
+        return
+    cmd = event.raw_text.split(" ", 1)
+    if len(cmd) == 1:
+        await event.reply("Usage: `/sh <command>`")
+        return
+    msg = await event.reply("Executing `{}`...".format(cmd[1]))
+    ps = await asyncio.create_subprocess_shell(cmd[1], stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await ps.communicate()
+    output = "**Command:**\n`{}`\n\n".format(cmd[1])
+    if stdout:
+        output += "**stdout:**\n`{}`\n\n".format(stdout.decode().strip())
+    if stderr:
+        output += "**stderr**:\n`{}`".format(stderr.decode().strip())
+    if len(output) > 4096:
+        await msg.edit("Output too long, sending as file...")
+        output_ = output.replace("`", "").replace("*", "")
+        with io.BytesIO(output_.encode()) as f:
+            f.name = "output.txt"
+            await client.send_file(event.chat_id, file=f, reply_to=event.id)
+        await msg.delete()
+    else:
+        await msg.edit(output)
+
+@client.on(events.NewMessage(pattern="^/eval"))
+async def eval_action(event):
+    if not db.isAuth(event.sender_id):
+        return
+    code = event.raw_text.split(" ", 1)
+    if len(code) == 1:
+        await event.reply("Usage: `/eval <code>`")
+        return
+    msg = await event.reply("Running...")
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
     try:
-        t = asyncio.create_task(aexec(cmd, event))
-        await t
+        value = await aexec(code[1], event)
     except Exception:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
+    evaluation = exc or stderr or stdout or value or "No output"
+    output = "**Code:**\n`{}`\n\n**Output:**\n`{}`".format(code[1], evaluation)
+    if len(output) > 4096:
+        await msg.edit("Output too long, sending as file...")
+        output_ = output.replace("`", "").replace("*", "")
+        with io.BytesIO(output_.encode()) as f:
+            f.name = "output.txt"
+            await client.send_file(event.chat_id, file=f, reply_to=event.id)
+        await msg.delete()
     else:
-        evaluation = "Success"
-    final_output = (
-        f"**•  Eval : **\n```{cmd}``` \n\n**•  Result : **\n```{evaluation}``` \n"
-    )
-    try:
-        await catevent.edit(final_output)
-    except:
-        with io.open("output.txt", "w", encoding="utf-8") as k:
-            k.write(str(final_output).replace("`", "").replace("*", ""))
-            k.close()
-        await event.client.send_file(event.chat_id, "output.txt")
-        os.remove('output.txt')
-        await catevent.delete()
-    LOGGER.info(f"Eval: {cmd}\nExcute by: {event.sender_id}")
+        await msg.edit(output)
 
-# --- BASH DEF HERE --- #
-async def bash(cmd):
-
-    process = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    err = stderr.decode().strip()
-    out = stdout.decode().strip()
-    return out, err
-
-# --- BASH EVENT HERE --- #
-@client.on(events.NewMessage(chats=auth_chts, pattern="/bash ?(.*)"))
-async def bashE(event):
-    if event.sender_id in banned_usrs:
-        return await event.respond("You are Banned!")
-    cmd = "".join(event.message.message.split(maxsplit=1)[1:])
-    oldmsg = await event.respond("`Running...`")
-    out, err = await bash(cmd)
-    LOGGER.info(f"Bash: {cmd}\nExcute by: {event.sender_id}")
-    if not out:
-        out = None
-    elif not err:
-        err = None
-    try:
-        await oldmsg.edit(f'**CMD:** `{cmd}`\n**ERROR:**\n `{err}`\n**OUTPUT:**\n `{out}`')
-    except:
-        with io.open("output.txt", "w", encoding="utf-8") as k:
-            k.write(f'CMD: {cmd}\nERROR:\n {err}\nOUTPUT:\n {out}')
-            k.close()
-        await event.client.send_file(event.chat_id, "output.txt", reply_to=event)
-        os.remove('output.txt')
-        await oldmsg.delete()
-
-print('>> BOT STARTED <<')
-os.system("python -V")
+client.start(bot_token=getEnv("BOT_TOKEN"))
+print("=== STARTED ===")
 client.run_until_disconnected()
-
-
